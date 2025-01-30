40f6d9b81762612548cd6209df1b002c979c2572
Suraj Singh <yousurajsingh@gmail.com>
2025-01-30 21:29:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-//your code here!
+
+
+var listItem = document.querySelector('#infi-list');
+
+var nextItem = 1;
+var loadMore = function(){
+    for(var i = 1; i <= 20; i++){
+        var item = document.createElement('li');
+        item.innerText = 'Item ' + nextItem;
+        listItem.appendChild(item);
+        nextItem++;
+    }
+}
+
+listItem.addEventListener('scroll', function(){
+    if(listItem.scrollTop + listItem.clientHeight >= listItem.scrollHeight){
+        loadMore();
+    }
+});
+
+
+loadMore();
 
